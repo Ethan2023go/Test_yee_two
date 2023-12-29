70442c77a02d47517d2b722a518531b31dab2898
Ethan <easonbest123@gmail.com>
2023-12-29 10:32:15 +0800

    簡單超連結部分 & 跑馬燈

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 ﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <!-- saved from url=(0039) -->
-<html xmlns="http://www.w3.org/1999/xhtml"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 
 <title>健康促進網</title>
 <link href="./css/css.css" rel="stylesheet" type="text/css">
@@ -15,26 +17,29 @@
 <iframe name="back" style="display:none;"></iframe>
 	<div id="all">
     	<div id="title">
-        00 月 00 號 Tuesday | 今日瀏覽: 1 | 累積瀏覽: 36        </div>
-        <div id="title2">
-        	
+        <?=date("m月d日 l");?> | 今日瀏覽: 1 | 累積瀏覽: 36       
+	     <a href="index.php" style='float:right'>回首頁</a>
+	</div>
+        <div id="title2" title='健康促進網-回首頁'>
+        	<img src="./icon/02B01.jpg" alt="">
         </div>
         <div id="mm">
         	<div class="hal" id="lef">
-            	                	    <a class="blo" href="?do=po">分類網誌</a>
-               	                     	    <a class="blo" href="?do=news">最新文章</a>
-               	                     	    <a class="blo" href="?do=pop">人氣文章</a>
-               	                     	    <a class="blo" href="?do=know">講座訊息</a>
-               	                     	    <a class="blo" href="?do=que">問卷調查</a>
-               	                 </div>
+                  <a class="blo" href="?do=po">分類網誌</a>
+                  <a class="blo" href="?do=news">最新文章</a>
+                  <a class="blo" href="?do=pop">人氣文章</a>
+                  <a class="blo" href="?do=know">講座訊息</a>
+                  <a class="blo" href="?do=que">問卷調查</a>
+            </div>
             <div class="hal" id="main">
             	<div>
-            		
-                	<span style="width:18%; display:inline-block;">
-                    	                    	<a href="?do=login">會員登入</a>
-                    	                    </span>
+            		<marquee style="width:78%;">請民眾踴躍投稿電子報，讓電子報成為大家互相交流、分享的園地!詳見最新文章</marquee>
+                	<span style="width:20%; display:inline-block;">
+                    	<a href="?do=login">會員登入</a>
+                    </span>
                     	<div class="">
-                		                        </div>
+                		
+					</div>
                 </div>
             </div>
         </div>
