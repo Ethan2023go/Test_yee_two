4cd539c21f919902acc7bbe484d539c13f7e5d89
Ethan <easonbest123@gmail.com>
2024-01-05 09:44:48 +0800

    將標籤與下方段落使用js做出連結並顯示

--- front/main.php
+++ front/main.php
@@ -26,13 +26,14 @@
 
 
 <div class="tags">
-    <div class="tag active">健康新知</div>
-    <div class="tag">菸害防治</div>
-    <div class="tag">癌症防治</div>
-    <div class="tag">慢性病防治</div>
+    <div id="sec01" class="tag active">健康新知</div>
+    <div id="sec02" class="tag">菸害防治</div>
+    <div id="sec03" class="tag">癌症防治</div>
+    <div id="sec04" class="tag">慢性病防治</div>
 </div>
+
 <article>
-    <section id="section01"><h2>健康新知</h2></section>
+    <section id="section01" style="display:block"><h2>健康新知</h2></section>
     <section id="section02"><h2>菸害防治</h2></section>
     <section id="section03"><h2>癌症防治</h2></section>
     <section id="section04"><h2>慢性病防治</h2></section>
@@ -42,6 +43,8 @@
    $(".tag").on('click',function(){
         $(".tag").removeClass('active')
         $(this).addClass('active')
-        let id =$(this)
+        let id=$(this).attr('id').replace("sec",'section');
+        $("section").hide();
+        $("#"+id).show();
    })
 </script>
