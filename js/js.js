421939ead03330fc4520d17abd6b2d66b8a7ca86
Ethan <easonbest123@gmail.com>
2024-01-08 14:14:00 +0800

    讚與收回讚功能 省流量方法嘗試

--- js/js.js
+++ js/js.js
@@ -5,7 +5,22 @@ function lo(th,url)
 }
 function good(news) {  
 	$.post("./api/good.php",{news},()=>{
-		// location.reload();  reload方式 很消耗流量
+		location.reload(); 
+		// reload方式 很消耗流量 (但是考試很快)
+		
+		
+		// 下面這個方法屬於省流方法
+
+		// switch($("#n"+news).text()){
+		// 	case "讚":
+		// 		$("#n"+news).text("收回讚")
+		// 		$("#g"+news).text($("#g"+news).text()*1+1)
+		// 	break;
+		// 	case "收回讚":
+		// 		$("#n"+news).text("讚")
+		// 		$("#g"+news).text($("#g"+news).text()*1-1)
+		// 	break;
+		// }
         
 	})  
   }
